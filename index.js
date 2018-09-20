cd584d3ff59915e787de0bc525dace06e3124b14
meg768 <meg768@users.noreply.github.com>
2018-09-20 21:41:33 +0200

    -

--- index.js
+++ index.js
@@ -65,45 +65,42 @@ class TiltHydrometer {
 
         this.enableTimer();
 
+        this.enableTilt();
+
+
+
+    }
+
+
+    enableTilt() {
 
         Bleacon.on('discover', (bleacon) => {
 
             // Identifies the TILT Hydrometer available
             var tilt = {
-                "a495bb10c5b14b44b5121370f02d74de": "Red",
-                "a495bb20c5b14b44b5121370f02d74de": "Green",
-                "a495bb30c5b14b44b5121370f02d74de": "Black",
-                "a495bb40c5b14b44b5121370f02d74de": "Purple",
-                "a495bb50c5b14b44b5121370f02d74de": "Orange",
-                "a495bb60c5b14b44b5121370f02d74de": "Blue",
-                "a495bb70c5b14b44b5121370f02d74de": "Pink"
+                "a495bb10c5b14b44b5121370f02d74de": "red",
+                "a495bb20c5b14b44b5121370f02d74de": "green",
+                "a495bb30c5b14b44b5121370f02d74de": "black",
+                "a495bb40c5b14b44b5121370f02d74de": "purple",
+                "a495bb50c5b14b44b5121370f02d74de": "orange",
+                "a495bb60c5b14b44b5121370f02d74de": "blue",
+                "a495bb70c5b14b44b5121370f02d74de": "pink"
             };
 
-            function buildPayload(bleacon){
 
-                var deviceLabel = tilt[bleacon.uuid] + '-' + bleacon.uuid; // Assigns the device label based on the TILT identified
-                bleacon.timeStamp = Date.now(); // Set the actual timestamp
-
-                // Build the payload by default
-                var payload = {
-                    "uuid": deviceLabel,
-                    "temperature":{ "value": bleacon.major, "timestamp": bleacon.timeStamp },
-                    "gravity": { "value": bleacon.minor/1000, "timestamp": bleacon.timeStamp },
-                    "rssi": { "value": bleacon.rssi, "timestamp": bleacon.timeStamp }
-                };
-                return payload;
-            }
-
-            if (tilt[bleacon.uuid] != null) {
-                this.log(buildPayload(bleacon));
+            if (tilt[bleacon.uuid] == this.config.color) {
+                var payload = {};
+                payload.timestamp = new Date();
+                payload.color = this.config.color;
+                payload.temperature = bleacon.major;
+                payload.gravity = bleacon.minor / 1000;
+                payload.rssi = bleacon.rssi;
+                this.log(payload);
             }
         });
 
         Bleacon.startScanning();
-
-
     }
-
 
 
     disableTimer() {
@@ -161,19 +158,22 @@ class TiltHydrometer {
 
         var calls = undefined;
 
-        switch(state) {
-            case Characteristic.CurrentHeatingCoolingState.OFF: {
-                calls = this.config.state.off;
-                break;
-            };
-            case Characteristic.CurrentHeatingCoolingState.HEAT: {
-                calls = this.config.state.heat;
-                break;
-            };
-            case Characteristic.CurrentHeatingCoolingState.COOL: {
-                calls = this.config.state.cool;
-                break;
-            };
+        switch (state) {
+            case Characteristic.CurrentHeatingCoolingState.OFF:
+                {
+                    calls = this.config.state.off;
+                    break;
+                };
+            case Characteristic.CurrentHeatingCoolingState.HEAT:
+                {
+                    calls = this.config.state.heat;
+                    break;
+                };
+            case Characteristic.CurrentHeatingCoolingState.COOL:
+                {
+                    calls = this.config.state.cool;
+                    break;
+                };
         }
 
         if (calls != undefined && !isArray(calls))
@@ -187,11 +187,10 @@ class TiltHydrometer {
 
                 var request = new Request(call.url);
 
-                request.request(call.options).then(() => {
-                })
-                .catch((error) => {
-                    this.log(error);
-                })
+                request.request(call.options).then(() => {})
+                    .catch((error) => {
+                        this.log(error);
+                    })
             });
         }
 
@@ -205,8 +204,7 @@ class TiltHydrometer {
             state = Characteristic.CurrentHeatingCoolingState.HEAT;
         } else if (this.shouldTurnOnCooling()) {
             state = Characteristic.CurrentHeatingCoolingState.COOL;
-        }
-        else {
+        } else {
             state = Characteristic.CurrentHeatingCoolingState.OFF;
         }
 
@@ -230,8 +228,7 @@ class TiltHydrometer {
     }
 
 
-    getAccessoryInformation() {
-    }
+    getAccessoryInformation() {}
 
     enableCurrentHeatingCoolingState() {
 
