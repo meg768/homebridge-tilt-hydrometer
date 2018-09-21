0d0bacf2f37bf36a8b6831e8473fe20ffcac72aa
meg768 <meg768@users.noreply.github.com>
2018-09-21 19:58:14 +0200

    -

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ class TiltHydrometer {
         this.enableCoolingThresholdTemperature();
         this.enableHeatingThresholdTemperature();
 
-        this.enableSerialNumber();
+        //this.enableSerialNumber();
 
         this.enableTilt();
         this.restartTiltTimer();
